6a893e83571dfb4f1ef5a36d03c6539b99e68dc2
ANASTASIBM18 <AnastasiBM18@gcc.edu>
2024-02-19 14:08:52 -0500

    Change to Main - 2/19/2024 - 2:08 PM - Added line saying, "Hello"

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hi how are you");
         System.out.println("What's wrong?");
+        System.out.println("Hello");
     }
 }
